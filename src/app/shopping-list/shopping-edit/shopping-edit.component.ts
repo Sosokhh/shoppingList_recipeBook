7025d9a35e5af2ceaac3658cbb68b00f17f64635
Soso Khurtsidze <sosokhurtsidzee@gmail.com>
2023-05-12 20:09:41 +0400

    add dropdown and shopping list edit functionallity

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.scss']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements AfterViewInit{
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
+  @ViewChild('nameInput') nameInput?: ElementRef;
+  @ViewChild('amountInput') amountInput?: ElementRef;
 
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    // console.log(this.nameInput)
+  }
+
+  onAddItem() {
+    const ingName = this.nameInput?.nativeElement?.value;
+    const ingValue = this.amountInput?.nativeElement.value;
+    const newIngredient = new Ingredient(
+      ingName, ingValue);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
