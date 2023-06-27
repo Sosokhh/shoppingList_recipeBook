3f9b4802b020633dc10c7e2d02684b80814279c3
Soso Khurtsidze <sosokhurtsidzee@gmail.com>
2023-06-28 00:06:50 +0400

    add http client

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,21 +10,28 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe('Another Test Recipe', 'This is simply a test',
-      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg',
-      [
-        new Ingredient('buns',2),
-        new Ingredient('meat',1)
-      ])
-  ]
+  // private recipes: Recipe[] = [
+  //   new Recipe('A Test Recipe', 'This is simply a test',
+  //     'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe('Another Test Recipe', 'This is simply a test',
+  //     'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg',
+  //     [
+  //       new Ingredient('buns',2),
+  //       new Ingredient('meat',1)
+  //     ])
+  // ]
+  private recipes: Recipe[] = [];
+
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
